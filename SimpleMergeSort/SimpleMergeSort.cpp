4a9ce206375eed6c97b7937e88dc81efd94755ee
Porcelain-vip <Porcelain.vip@qq.com>
2017-04-01 18:05:52 +0800

    优化代码

--- SimpleMergeSort/SimpleMergeSort.cpp
+++ SimpleMergeSort/SimpleMergeSort.cpp
@@ -1,18 +1,19 @@
 #include<iostream>
 #include<memory>
+using namespace std;
 
 template<typename Type>
-void simplemergesorthelp(Type* a, int left, int right)
+void SimpleMergeSortHelp(Type* a, int left, int right)
 {
 	if (left < right)
 	{
-		int mid = (left + right) / 2;
-		simplemergesorthelp<Type>(a, left, mid);
-		simplemergesorthelp<Type>(a, mid + 1, right);
+		int mid = left + (right - left) / 2;                                                       //also      int mid = (left + right) / 2;
+		SimpleMergeSortHelp<Type>(a, left, mid);                            //sort left part
+		SimpleMergeSortHelp<Type>(a, mid + 1, right);                  //sort right part
 
-		int i, low, high;
-		std::unique_ptr<Type[]> b(new Type[right + 1]);
-		for (i = left, low = left, high = mid + 1; low <= mid && high <= right;)
+		int i = left, low = left, high = mid + 1;
+		unique_ptr<Type[]> b(new Type[right + 1]);
+		for (; low <= mid && high <= right;)                                             //merge two parts
 		{
 			if (a[low] <= a[high]) { b[i] = a[low]; ++low; }
 			else { b[i] = a[high]; ++high; }
@@ -25,19 +26,18 @@ void simplemergesorthelp(Type* a, int left, int right)
 }
 
 template<typename Type>
-void simplemergesort(Type* a, int n)
+void SimpleMergeSort(Type* a, int n)
 {
-	simplemergesorthelp<Type>(a, 0, n - 1);
+	SimpleMergeSortHelp<Type>(a, 0, n - 1);
 }
 
 int main()
 {
-	using namespace std;
 	int a[10];
 	cout << "Please enter 10 numbers as you like: ";
 	for (int& value : a) cin >> value;
-	simplemergesort<int>(a, 10);
+	SimpleMergeSort<int>(a, 10);
 	cout << "Low-to-High: ";
 	for (int& value : a) cout << value << "   "; cout << endl;
-	return 0;
+	return EXIT_SUCCESS;
 }
